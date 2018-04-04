00a3d606d52c940d741d7eec9ed890e2d984aba1
Joan56 <sjoanandrez@gmail.com>
2018-04-04 13:54:09 +0200

    nuevo programa

---
+++ hola_mundo_joan.py
@@ -0,0 +1,9 @@
+var1 = input ("Digame un numero")
+var2 = input ("Digame otro numero")
+var3 = input ("Digame un tercer numero")
+
+if var1 > 0 
+print ("Este nummero no me vale")
+
+if var2 > var1 
+print ("Este numero no me vale")
